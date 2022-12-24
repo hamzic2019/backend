de91b83cd0614efafce67572fc69c42ed31c2a05
hamzic2019 <hamzicharis96@gmail.com>
2022-12-24 17:37:03 +0100

    sfdg

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://dynamic-manatee-3cc1fa.netlify.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://dynamic-manatee-3cc1fa.netlify.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
